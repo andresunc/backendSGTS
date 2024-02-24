6bea07952ab48191819cc5e06e51a82ca31c71cb
Andrés <sergio.andres.garcia@mi.unc.edu.ar>
2024-02-24 19:23:30 -0300

    Add EmpresaDto

---
+++ SGTS/src/main/java/Backend/SGTS/Repository/Dto/DtoRepositoryEmpresa.java
@@ -0,0 +1,37 @@
+package Backend.SGTS.Repository.Dto;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import Backend.SGTS.Constants.SqlQueriesEmpresa;
+import Backend.SGTS.Entity.Dto.EmpresaDto;
+
+@Repository
+public class DtoRepositoryEmpresa {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	private SqlQueriesEmpresa sqlQueries = new SqlQueriesEmpresa();
+	
+	public List<EmpresaDto> obtenerEmpresaDto() {
+
+		// Obtener empresas.
+		String sql = sqlQueries.getEmpresaDto();
+
+		return jdbcTemplate.query(sql, (resultSet, rowNum) -> {
+			EmpresaDto empresaDto = new EmpresaDto();
+			empresaDto.setIdRubro(resultSet.getInt("idRubro"));
+			empresaDto.setRubro(resultSet.getString("rubro"));
+			empresaDto.setIdRiesgo(resultSet.getInt("idRiesgo"));
+			empresaDto.setRiesgo(resultSet.getString("riesgo"));
+			empresaDto.setCliente(resultSet.getString("cliente"));
+			empresaDto.setCuit(resultSet.getString("cuit"));
+			empresaDto.setDireccion(resultSet.getString("direccion"));
+			
+			return empresaDto;
+		});
+	}
+}

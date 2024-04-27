c4a5e85329545d1ca393f7544a016a01c8a3d879
Andrés <sergio.andres.garcia@mi.unc.edu.ar>
2024-04-27 18:44:37 -0300

    fix DeleteMapping

--- SGTS/src/main/java/Backend/SGTS/Controller/EstadoController.java
+++ SGTS/src/main/java/Backend/SGTS/Controller/EstadoController.java
@@ -3,6 +3,7 @@ package Backend.SGTS.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -66,7 +67,7 @@ public class EstadoController {
 	}
 	
 	// Método para eliminar un estado de manera lógica
-	@PostMapping("/delete/{id}")
+	@DeleteMapping("/delete/{id}")
 	public void delete(@PathVariable Integer id) {
 		estadoService.delete(id);
 	}

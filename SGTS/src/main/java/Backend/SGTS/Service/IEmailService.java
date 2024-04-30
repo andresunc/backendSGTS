9706382bff3b75a2904f666ba3ee89f6c9604729
matias <matiasjbro@gmail.com>
2024-04-30 00:00:37 -0300

    se agrega la funcion de envio de mail con o sin adjunto

---
+++ SGTS/src/main/java/Backend/SGTS/Service/IEmailService.java
@@ -0,0 +1,10 @@
+package Backend.SGTS.Service;
+
+import java.io.File;
+
+public interface IEmailService {
+
+    void sendEmail(String[] toUser, String subject, String message);
+
+    void sendEmailWithFile(String[] toUser, String subject, String message, File file);
+}

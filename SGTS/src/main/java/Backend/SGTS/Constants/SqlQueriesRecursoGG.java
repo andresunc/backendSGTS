b2c2f8a87a2234044d3127058c342713ce90aa0a
Andrés <sergio.andres.garcia@mi.unc.edu.ar>
2024-05-21 21:37:49 -0300

    punto 1 21524

--- SGTS/src/main/java/Backend/SGTS/Constants/SqlQueriesRecursoGG.java
+++ SGTS/src/main/java/Backend/SGTS/Constants/SqlQueriesRecursoGG.java
@@ -7,10 +7,13 @@ public class SqlQueriesRecursoGG {
             + "per.id_Persona as 'idPersona',"
             + "per.Nombre as 'nombre',"
             + "per.Apellido as 'apellido',"
-            + "per.Dni as 'dni'"
+            + "per.Dni as 'dni',"
+            + "rol.rol as 'rol'"
             + "FROM recurso_gg rgg "
             + "LEFT JOIN persona per on per.id_Persona = rgg.Persona_id_Persona "
             + "LEFT JOIN usuario us ON rgg.id_recurso_gg = us.Recurso_GG_id_Recurso_GG "
+            + "LEFT JOIN usuario_rol usr ON usr.id_usuario = us.id_usuario "
+            + "LEFT JOIN rol ON rol.id_rol = usr.id_rol "
             + "WHERE rgg.eliminado = 0";
 
     public SqlQueriesRecursoGG() {
